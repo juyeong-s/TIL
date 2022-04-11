649a06413a93cdb9e6e09453003f73fc0820d79c
juyeong-chungbuk <happyu9986@gmail.com>
2022-04-11 23:10:05 +0900

    💻 Upload TIL`s posting

---
+++ docs/자바스크립트/closure-2.md
@@ -0,0 +1,10 @@
+---
+layout: page
+title: 클로저의 활용
+parent: 자바스크립트
+nav_order: 14
+has_children: false
+permalink: /js/closure-2/
+---
+
+# 클로저의 활용
--- docs/자바스크립트/test.js
+++ docs/자바스크립트/test.js
@@ -1,58 +1,96 @@
-function b(num){
-    return num*num;
-}
+// function b(num){
+//     return num*num;
+// }
 
-function a(func, n1, n2){
-    return func(n2);
-}
+// function a(func, n1, n2){
+//     return func(n2);
+// }
 
-// console.log(a(b, 2, 3));
+// // console.log(a(b, 2, 3));
 
-function add(num1) {
-    return function(num2) {
-        return num1 + num2;
-    }
-}
+// function add(num1) {
+//     return function(num2) {
+//         return num1 + num2;
+//     }
+// }
 
-console.log(add(3)(4));
+// console.log(add(3)(4));
 
-const x = 1;
+// const x = 1;
 
-function outerFunc() {
-    const x = 10;
+// function outerFunc() {
+//     const x = 10;
 
-    function innerFunc() {
-        console.log(x); // 10
-    }
+//     function innerFunc() {
+//         console.log(x); // 10
+//     }
 
-    innerFunc();
-}
+//     innerFunc();
+// }
 
-outerFunc();
+// outerFunc();
 
-const x = 1;
+// const x = 1;
+
+// function foo() {
+//     const x = 10;
+//     bar();
+// }
+
+// function bar() {
+//     console.log(x);
+// }
+
+// foo();  // ?
+// bar();  // ?
+
+// const x = 1;
+
+// // 1번
+// function outer() {
+//     const x = 10;
+//     const inner = function () { console.log(x) };   // 2번
+//     return inner;
+// }
+
+// // outer함수를 호출하면 중첩함수 inner를 반환한다.
+// const innerFunc = outer();  // 3번
+// innerFunc();    // 4번 -> 10
 
 function foo() {
-    const x = 10;
-    bar();
+  const x = 1;
+  const y = 2;
+
+  function bar() {
+    const z = 3;
+    console.log(z);
+  }
+  return bar;
 }
 
-function bar() {
+const bar = foo();
+bar();
+
+function foo() {
+  const x = 1;
+
+  function bar() {
     console.log(x);
+  }
+  bar();
 }
 
-foo();  // ?
-bar();  // ?
+foo();
 
-const x = 1;
+function foo() {
+  const x = 1;
+  const y = 2;
 
-// 1번
-function outer() {
-    const x = 10;
-    const inner = function () { console.log(x) };   // 2번
-    return inner;
+  function bar() {
+    console.log(x);
+  }
+  return bar;
 }
 
-// outer함수를 호출하면 중첩함수 inner를 반환한다.
-const innerFunc = outer();  // 3번
-innerFunc();    // 4번 -> 10
+const bar = foo();
+bar();
